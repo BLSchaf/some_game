645dff9496ba6c3d6e9f8366c29fc0f3345bc722
Dolphin <johannes--sommer@web.de>
2020-08-05 23:32:15 +0200

    quick syntax fix

--- helper_drone.py
+++ helper_drone.py
@@ -14,7 +14,7 @@ main_clock = pygame.time.Clock()
 WINDOW = pygame.display.set_mode(SIZE)
 pygame.display.set_caption('Helper Drone')
 
-TITLE_IMG = pygame.image.load(os.path.join('assets', 'HD_title.png'))
+TITLE_IMG = pygame.image.load('assets\HD_title.png')
 PLAY_BUTTON_UP = pygame.image.load(os.path.join('assets', 'HD_title_play_up.png')).convert()
 PLAY_BUTTON_DOWN = pygame.image.load(os.path.join('assets', 'HD_title_play_down.png')).convert()
 
@@ -62,50 +62,54 @@ class Line():
         pygame.draw.line(WINDOW, self.color, self.start, self.end)
 
 
-def slope(p1, p2) :
-   return (p2[1] - p1[1]) * 1. / (p2[0] - p1[0]) # y = mx + b | m := slope
+
+# Intersect Stuff ------------------------------------------------- #
+def slope(p1, p2):
+    print(p2[0], p1[0])
+    return (p2[1] - p1[1])*1. / (p2[0] - p1[0]) # y = mx + b | m := slope
    
-def y_intercept(slope, p1) :
-   return p1[1] - 1.  slope  p1[0] # y = mx+b -> b = y-mb | b:= y_intercept
+def y_intercept(slope, p1):
+    return p1[1] - 1.*slope * p1[0] # y = mx+b -> b = y-mb | b:= y_intercept
 
 def intersect(line1, line2) :
-   min_allowed = 1e-5   # guard against overflow
-   big_value = 1e10     # use instead (if overflow would have occurred)
-   m1 = slope(line1[0], line1[1])
-   b1 = y_intercept(m1, line1[0])
-   
-   m2 = slope(line2[0], line2[1])
-   b2 = y_intercept(m2, line2[0])
-   
-   if abs(m1 - m2) < min_allowed:
+    min_allowed = 1e-5   # guard against overflow
+    big_value = 1e10     # use instead (if overflow would have occurred)
+    m1 = slope(line1[0], line1[1])
+    b1 = y_intercept(m1, line1[0])
+
+    m2 = slope(line2[0], line2[1])
+    b2 = y_intercept(m2, line2[0])
+
+    if abs(m1 - m2) < min_allowed:
       x = big_value
-   else :
+    else :
       x = (b2 - b1) / (m1 - m2)
       
-   y = m1 * x + b1
-   #y2 = m2 * x + b2
-   return (int(x),int(y))
+    y = m1 * x + b1
+    #y2 = m2 * x + b2
+    return (int(x),int(y))
 
 def segment_intersect(line1, line2):
-   intersection_pt = intersect(line1, line2)
-   
-   if (line1[0][0] < line1[1][0]) :
+    intersection_pt = intersect(line1, line2)
+
+    if (line1[0][0] < line1[1][0]) :
       if intersection_pt[0] < line1[0][0] or intersection_pt[0] > line1[1][0]:
          return None
-   else :
+    else :
       if intersection_pt[0] > line1[0][0] or intersection_pt[0] < line1[1][0]:
          return None
          
-   if (line2[0][0] < line2[1][0]):
+    if (line2[0][0] < line2[1][0]):
       if intersection_pt[0] < line2[0][0] or intersection_pt[0] > line2[1][0]:
          return None
-   else :
+    else :
       if intersection_pt[0] > line2[0][0] or intersection_pt[0] < line2[1][0]:
          return None
 
-   return intersection_pt
+    return intersection_pt
 
 
+# Update Game Window ---------------------------------------------- #
 def update_window(drone, obstacle):
     '''
     Updates the window
